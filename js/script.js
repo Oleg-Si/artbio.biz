280fc1280d1ce5de6001d7a583f7f96f36ca46dd
OlegS <xwx18@mail.ru>
2019-08-07 23:59:01 +0300

    fin

--- js/script.js
+++ js/script.js
@@ -1,94 +1,148 @@
-const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.menu__btn');
-const slideSpeed = 700;
+$(document).ready(() => {
+  const menu = document.querySelector('.menu');
+  const menuBtn = document.querySelector('.menu__btn');
+  const slideSpeed = 700;
 
-menuBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  menu.classList.toggle('active');
-});
+  menuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.toggle('active');
+  });
 
-let screenHeight = window.innerHeight;
-let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
+  let screenWidth = window.innerWidth;
 
-$(window).resize(() => {
-  screenHeight = window.innerHeight;
-  screenWidth = window.innerWidth;
-});
+  $(window).resize(() => {
+    screenHeight = window.innerHeight;
+    screenWidth = window.innerWidth;
+  });
 
-if (screenWidth >= 1200 && screenHeight >= 680) {
-  let currentSlide = 1;
-  const slideCount = document.querySelector('.main').children.length;
-  let scrollValue = 0;
-
-  const nextSlide = (clearStatus) => {
-    if (currentSlide < slideCount) {
-      const height = screenHeight * currentSlide;
-      $('body,html').animate({scrollTop: height}, slideSpeed);
-      currentSlide++;
-      scrollValue += screenHeight;
-    };
-    clearStatus();
-  }
-  const beforeSlide = () => {
-    if (currentSlide > 1) {
-      const height = scrollValue - screenHeight;
-      $('body,html').animate({scrollTop: height}, slideSpeed);
-      currentSlide--;
-      scrollValue -= screenHeight;
-    };
-    clearStatus();
+  const slides = document.querySelectorAll('.main > section');
+  const slidesArr = Array.from(slides);
+  const cleanSlideStatus = () => {
+    slides.forEach((el) => {
+      el.classList.remove('active');
+    })
   }
 
-  let status = 0;
-
-  const clearStatus = () => {
-    setTimeout(() => {
-      status = 0;
-    }, slideSpeed)
-  }
-
-  $('.main').bind('mousewheel DOMMouseScroll', function(event){
-    event.preventDefault();
-    if (status == 0) {
-      if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        status = 1;
-        beforeSlide(clearStatus);
-      } else {
-        status = 1;
-        nextSlide(clearStatus);
-      }
+  slidesArr.some((el, i) => {
+    if (el.classList.contains('active')) {
+      const offset = el.getAttribute('data-offset');
+      $('body,html').animate({scrollTop: offset}, slideSpeed);
+      return el;
     }
   });
-}
 
-$(".menu__item").on("click","a", function (event) {
-  event.preventDefault();
+  if (screenWidth >= 1200 && screenHeight >= 680) {
 
-  $('.menu__item').removeClass('active');
-  const id  = $(this).attr('href');
-  const top = $(id).offset().top;
-  $('body,html').animate({scrollTop: top}, slideSpeed);
-  $(this).parent().addClass('active');
-  menu.classList.remove('active');
-});
+    slides.forEach((el, i) => {
+      el.setAttribute('data-offset', screenHeight * i);
+    });
 
-/* ---------------------------------------------------------------------------- */
+    const slideCount = slides.length;
 
-const products = document.querySelectorAll('.products__item');
-const overlay = document.querySelector('.overlay');
+    const changeMenu = (slideId) => {
+      $('.menu__item').removeClass('active');
+      const menuItems = menu.querySelectorAll('.menu__item');
+      const menuItemsArr = Array.from(menuItems);
+      menuItemsArr.some((el) => {
+        const link = el.getAttribute('data-link');
+        if (link == slideId) {
+          el.classList.add('active');
+        }
+      })
+    }
 
-products.forEach((el) => {
-  const btn = el.querySelector('.products__content');
-  btn.addEventListener('click', () => {
-    overlay.classList.add('overlay--open');
+    const nextSlide = (clearStatus) => {
+      slidesArr.some((el, i) => {
+        if (el.classList.contains('active')) {
+          const nextSlide = el.nextElementSibling;
+          if (nextSlide != null) {
+            const offset = nextSlide.getAttribute('data-offset');
+            const nextSlideId = nextSlide.getAttribute('id');
+            changeMenu(nextSlideId);
+            cleanSlideStatus();
+            $('body,html').animate({scrollTop: offset}, slideSpeed);
+            nextSlide.classList.add('active');
+          }
+          return el;
+        }
+      });
+      clearStatus();
+    }
+    const beforeSlide = () => {
+      slidesArr.some((el, i) => {
+        if (el.classList.contains('active')) {
+          const prevSlide = el.previousElementSibling;
+          if (prevSlide != null) {
+            const offset = prevSlide.getAttribute('data-offset');
+            const prevSlideId = prevSlide.getAttribute('id');
+            changeMenu(prevSlideId);
+            cleanSlideStatus();
+            $('body,html').animate({scrollTop: offset}, slideSpeed);
+            prevSlide.classList.add('active')
+          }
+          return el;
+        }
+      });
+      clearStatus();
+    }
 
-    const card = el.querySelector('.products__card').cloneNode(true);
-    card.classList.add('products__card--open');
-    overlay.appendChild(card);
+    let status = 0;
+
+    const clearStatus = () => {
+      setTimeout(() => {
+        status = 0;
+      }, slideSpeed)
+    }
+
+    $('.main').bind('mousewheel DOMMouseScroll', function(event){
+      event.preventDefault();
+      if (status == 0) {
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+          status = 1;
+          beforeSlide(clearStatus);
+        } else {
+          status = 1;
+          nextSlide(clearStatus);
+        }
+      }
+    });
+  }
+
+  $(".menu__item").on("click","a", function (event) {
+    event.preventDefault();
+    $('.menu__item').removeClass('active');
+
+    const id  = $(this).attr('href');
+    const link = $(this).parent().attr('data-link');
+    const top = $(id).offset().top;
+
+    cleanSlideStatus();
+    document.getElementById(link).classList.add('active');
+
+    $('body,html').animate({scrollTop: top}, slideSpeed);
+    $(this).parent().addClass('active');
+    menu.classList.remove('active');
+  });
+
+  /* ---------------------------------------------------------------------------- */
+
+  const products = document.querySelectorAll('.products__item');
+  const overlay = document.querySelector('.overlay');
+
+  products.forEach((el) => {
+    const btn = el.querySelector('.products__content');
+    btn.addEventListener('click', () => {
+      overlay.classList.add('overlay--open');
+
+      const card = el.querySelector('.products__card').cloneNode(true);
+      card.classList.add('products__card--open');
+      overlay.appendChild(card);
+    })
   })
-})
 
-overlay.addEventListener('click', () => {
-  overlay.classList.remove('overlay--open');
-  overlay.innerHTML = '';
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('overlay--open');
+    overlay.innerHTML = '';
+  });
 })
